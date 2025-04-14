b794b529b2b6310953416997cb983655c7f4e3c5
iftakhersifat <hossensifat615@gmail.com>
2025-04-15 00:57:09 +0600

    added grid

--- src/Components/Users/Use.jsx
+++ src/Components/Users/Use.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Use = ({data}) => {
     const {name, email, phone} = data;
     return (
-        <div className='text-center'>
+        <div className='text-center border border-white bg-amber-600 my-4 p-8 text-white rounded-2xl'>
             <h1>Name: {name}</h1>
             <h1>Email: {email}</h1>
             <h3>Phone Number: {phone}</h3>
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -8,7 +8,7 @@ const Users = () => {
     return (
         <div className='text-center mt-8'>
             <h1>users</h1>
-            <div>
+            <div className='p-4 grid grid-cols-2'>
                 {
                     useData.map(data=><Use data={data}></Use>)
                 }
