fede800b5c5ff59def518bf867ca998f090ab006
iftakhersifat <hossensifat615@gmail.com>
2025-04-15 17:12:40 +0600

    added hie show details conditon

--- src/Components/Comments/Comment.jsx
+++ src/Components/Comments/Comment.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 const Comment = ({comment}) => {
     const {id, name, email}=comment;
     const navigator =useNavigate();
-    const handelButton=()=>{
-        navigator(`/comments/${id}`)
-    }
+
+    // show hide
+    const [show, setShow] =useState(false);
+    // const handelButton=()=>{
+    //     navigator(`/comments/${id}`)
+    // }
     return (
         <div className='text-center border border-white bg-amber-600 m-4 p-8 text-white rounded-2xl'>
             <h1>Id : {id}</h1>
             <h1>Name : {name}</h1>
             <h1 className='mb-4'>Email : {email}</h1>
-            <button onClick={handelButton} className='bg-blue-600 rounded-xl px-2 py-2'>More Details</button>
+            <button onClick={()=>setShow(!show)}  className={`bg-blue-600 rounded-xl px-2 py-2 {show ? "Hide" : "Show"}`}> {show ? "Hide " : "show "}More Details
+                
+            </button>
         </div>
     );
 };
