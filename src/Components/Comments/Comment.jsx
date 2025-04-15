4153255f7938a2fdecfe4c752d6661434a3dad28
iftakhersifat <hossensifat615@gmail.com>
2025-04-15 16:50:18 +0600

    added comments

--- src/Components/Comments/Comment.jsx
+++ src/Components/Comments/Comment.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Comment = () => {
+const Comment = ({comment}) => {
+    const {id, name, email}=comment;
     return (
-        <div>
-            
+        <div className='text-center border border-white bg-amber-600 m-4 p-8 text-white rounded-2xl'>
+            <h1>Id : {id}</h1>
+            <h1>Name : {name}</h1>
+            <h1>Email : {email}</h1>
         </div>
     );
 };
--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
+import Comment from './Comment';
 
 const Comments = () => {
     const showComments =useLoaderData();
     console.log(showComments);
     return (
-        <div>
-            
+        <div className='text-center mt-8 border-b-2 border-gray-500'>
+            <h1 className='text-3xl font-bold text-gray-700'>All Comments</h1>
+            <div>
+                {
+                    showComments.map(comment=><Comment key={comment.id} comment={comment}></Comment>)
+                }
+            </div>
         </div>
     );
 };
